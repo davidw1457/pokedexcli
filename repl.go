aa9f8849a546e2f84048e9a6500ce2cab79e64d7
David Williams <davidw1457@gmail.com>
2025-08-07 09:39:42 -0500

    ch01.l05

--- repl.go
+++ repl.go
@@ -16,13 +16,42 @@ func startRepl() {
 			fmt.Println(scanner.Err())
 			os.Exit(1)
 		}
-		input := scanner.Text()
-		cleanedInput := cleanInput(input)
-		fmt.Printf("Your command was: %s\n", cleanedInput[0])
+		input := cleanInput(scanner.Text())
+        
+        if command, ok := getCommands()[input[0]]; ok {
+            err := command.callback()
+            if err != nil {
+                fmt.Println(err)
+            }
+        } else {
+            fmt.Println("Unknown command")
+        }
 	}
+
 }
 
 func cleanInput(text string) []string {
     words := strings.ToLower(text)
     return strings.Fields(words)
+}
+
+type cliCommand struct {
+    name string
+    description string
+    callback func() error
+}
+
+func getCommands() map[string]cliCommand {
+    return map[string]cliCommand{
+        "help": {
+            name: "help",
+            description: "Displays a help message",
+            callback: commandHelp,
+        },
+        "exit": {
+            name: "exit",
+            description: "Exit the Pokedex",
+            callback: commandExit,
+        },
+    }
 }
